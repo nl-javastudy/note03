ac2f24d06030371d48252b6a4505a9fc0b7d729b
www445 <103098617+Aquiran@users.noreply.github.com>
2025-05-02 18:32:53 +0900

    Update Main4.java

--- Chapter 03/Main4.java
+++ Chapter 03/Main4.java
@@ -3,10 +3,10 @@ import java.util.*;
 class ErrorTest2{
     private Optional<String> a = Optional.empty(); //비어있는 optional 객체 생성
     void printLen() {
-        System.out.println(a.map(str -> str.length()).orElse(0)); // Optional 객체 내부의 값을 변환하여 결과를 새로운 Optional 객체로 반환.
+        System.out.println(a.map(str -> str.length()).orElse(0)); // map = Optional 객체 내부의 값을 변환하여 결과를 새로운 Optional 객체로 반환.
     }
     void printStr() {
-        System.out.println(a.orElse("null").toString()); // Optional 객체가 비어있다면, 전달된 기본값 other를 반환.
+        System.out.println(a.orElse("null").toString()); // orElse = Optional 객체가 비어있다면, 전달된 기본값 other를 반환.
     }
     boolean isA() {
         return a.map(val -> val.equals("A")).orElse(false);
